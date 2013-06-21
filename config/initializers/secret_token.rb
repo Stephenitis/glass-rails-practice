64e6b1d095feeaa845f5b1be3ebb45793d6e8a54
Stephen Nguyen <nguyenhstephen@gmail.com>
2013-06-20 22:59:37 -0700

    created rails application and generated devise user model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Googleglassapp::Application.config.secret_token = 'd315fc883c0ed91f535b95f88e1aae336bac760378fff2888a97dbeab26e05baf61b33cd635440048687e0015ceaef96cead8ddfa61a2cfe0e615bab2ffc3a9b'
